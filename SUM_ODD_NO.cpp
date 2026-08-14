05cf0ecee0d8a8e3ede5d5cb55380ae101c1d64f
SAKET-SUMAN <notsaket8@gmail.com>
2026-08-14 22:39:07 +0530

    Add files via upload
    This code will take an integer as user input and print the sum of all the odd numbers till that number , by using loop.

---
+++ SUM_ODD_NO.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int a,i=1,sum=0;
+   cin>>a;
+   for(i=1;i<=a;i++){
+      if(i%2!=0){
+         sum+=i;
+      }
+   }
+   cout<<sum;
+}
